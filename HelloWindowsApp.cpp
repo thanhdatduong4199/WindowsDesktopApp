be8ba88d0551442ad63a361efa3a37dea09ac9a1
unknown <thanhdatduong4199@gmail.com>
2023-05-21 13:08:54 -0400

    use ccomptr

--- HelloWindowsApp.cpp
+++ HelloWindowsApp.cpp
@@ -4,6 +4,7 @@
 #include <windows.h>
 #include "BaseWindow.h"
 #include <shobjidl.h> 
+#include <atlbase.h> // Contains the declaration of CComPtr.
 
 
 
@@ -44,7 +45,7 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 	return TRUE;
 }
 
-
+/*
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int
 	nCmdShow)
 {
@@ -63,7 +64,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int
 	}
 	return 0;
 }
-
+*/
 
 /*
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int
@@ -108,3 +109,43 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int
 	return 0;
 }
 */
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int
+	nCmdShow)
+{
+	HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |
+		COINIT_DISABLE_OLE1DDE);
+	if (SUCCEEDED(hr))
+	{
+		CComPtr<IFileOpenDialog> pFileOpen;
+		// Create the FileOpenDialog object.
+		hr = pFileOpen.CoCreateInstance(__uuidof(FileOpenDialog));
+		if (SUCCEEDED(hr))
+		{
+			// Show the Open dialog box.
+			hr = pFileOpen->Show(NULL);
+			// Get the file name from the dialog box.
+			if (SUCCEEDED(hr))
+			{
+				CComPtr<IShellItem> pItem;
+				hr = pFileOpen->GetResult(&pItem);
+				if (SUCCEEDED(hr))
+				{
+					PWSTR pszFilePath;
+					hr = pItem->GetDisplayName(SIGDN_FILESYSPATH,
+						&pszFilePath);
+					// Display the file name to the user.
+					if (SUCCEEDED(hr))
+					{
+						MessageBox(NULL, pszFilePath, L"File Path", MB_OK);
+						CoTaskMemFree(pszFilePath);
+					}
+				}
+				// pItem goes out of scope.
+			}
+			// pFileOpen goes out of scope.
+		}
+		CoUninitialize();
+	}
+	return 0;
+}
